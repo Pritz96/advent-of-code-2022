352ae9fc6c058eca81f0ebfe6dc7192186578a00
Pritam <pritam1996@hotmail.co.uk>
2022-12-01 20:56:55 +0000

    initial commit solving question 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # advent-of-code-2022
-My solutions to advent of code 2022 written in Python
+My solutions to Advent of Code 2022 written in Python 3.
+
+A solution can be run by going into the directory of a question and running `python3 question<question number>.py`
---
+++ question01/question01.py
@@ -0,0 +1,22 @@
+# Solution to Question 1
+
+with open('puzzle_input.txt') as puzzle_input:
+    # Store total calories for each elf in an array
+    total_calories_array = []
+    # The total calories for one elf
+    total_calories = 0
+    # Loop through the lines of the text file
+    for line in puzzle_input:
+        # If there is a line break then add the total_calories for the previous elf into the total_calories_array and reset total_calories to 0
+        if len(line.strip()) == 0:
+            total_calories_array.append(total_calories)
+            total_calories = 0
+        # If there is not line break then add the line (as an integer) to the total_calories
+        else: 
+            total_calories = total_calories + int(line.strip())
+
+# Sort the total_calories array from highest to lowest 
+total_calories_array.sort(reverse=True)
+
+print("The Elf carrying the most calories is carrying a total of {} calories".format(total_calories_array[0]))
+print("The top three Elves are carrying a total of {} calories".format(total_calories_array[0] + total_calories_array[1] + total_calories_array[2]))
