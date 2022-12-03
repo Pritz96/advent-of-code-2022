9714f21486cbc49d5e2382750a29da9cfba9275f
Pritam <pritam1996@hotmail.co.uk>
2022-12-03 22:13:01 +0000

    updated comments for question 3 and added gitignore file

--- question03/question03.R
+++ question03/question03.R
@@ -4,13 +4,12 @@ library(dplyr)
 
 # Read in the puzzle input file as a CSV
 puzzle_input <- read.csv('puzzle_input.csv', header=FALSE)
-# Define a column name
 colnames(puzzle_input) <- c("rucksack_contents")
 # Change the rucksack_content column from a factor w/ levels to a character column
 puzzle_input$rucksack_contents <- as.character(puzzle_input$rucksack_contents)
 
 # For a given letter a-z or A-Z, this function returns the index of that letter
-# i.e. priority_letter('a') will return 1, priority_letter('B') will return 28
+# i.e. calculate_priority_number('a') will return 1, calculate_priority_number('B') will return 28
 calculate_priority_number <- function(priority_letter){
   priorities <- c(letters, LETTERS)
   return(match(priority_letter,priorities))
@@ -29,8 +28,7 @@ calculate_priority_letter <- function(first_compartment, second_compartment) {
   return(common_item)
 }
 
-# Splits the rucksack into the items in the first and second compartment
-# Finds the common priority letter and corresponding number 
+# Create new columns for the items in the first and second compartment, then new columns containing the priority letter and number
 puzzle_input_part1 <- puzzle_input %>% 
   dplyr::mutate(first_compartment = substr(puzzle_input$rucksack_contents, 1, nchar(puzzle_input$rucksack_contents)/2), 
                 second_compartment = substr(puzzle_input$rucksack_contents, nchar(puzzle_input$rucksack_contents)/2+1, nchar(puzzle_input$rucksack_contents)),
@@ -55,7 +53,7 @@ calculate_priority_per_group <- function(elf_group){
   return(priority)
 }
 
-rucksack <- as.vector(puzzle_input$rucksack_contents)
+rucksack <- puzzle_input$rucksack_contents
 # Split the elves into groups of 3 (not 100% sure how this line works)
 elf_groups <- split(rucksack, ceiling(seq_along(rucksack)/3))
 # For each group calculate their priority and store in the priorities vector
